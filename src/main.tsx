2e8404ee2b9857c570e5eff21cb141d49c84463b
jindalcozee <jindalcozee@github.com>
2026-04-01 05:26:25 +0530

    Revert: remove react-snap due to Vercel build environment limits

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import {StrictMode} from 'react';
-import {createRoot, hydrateRoot} from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-const rootElement = document.getElementById('root')!;
-
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(rootElement, <StrictMode><App /></StrictMode>);
-} else {
-  createRoot(rootElement).render(<StrictMode><App /></StrictMode>);
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
